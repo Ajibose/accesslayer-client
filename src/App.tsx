dd40515ccc8f3e1636194449a726aaf825f2ae2e
baedboibidex-cmyk <baedboibidex-cmyk@users.noreply.github.com>
2026-08-01 00:16:07 +0100

    fix: restore useNavigationTiming import in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import AppErrorBoundary from './components/common/AppErrorBoundary';
+import { useNavigationTiming } from './hooks/useNavigationTiming';
 import { routes } from './routes';
 import { useRouteChangeLogging } from './hooks/useRouteChangeLogging';
 
